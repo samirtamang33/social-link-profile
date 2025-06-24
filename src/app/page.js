a5100e1c1ceb8537e930da7d214ae953a01ebbab
samirtamang33 <152047704+samirtamang33@users.noreply.github.com>
2025-06-24 22:15:47 +0545

    complete

--- src/app/page.js
+++ src/app/page.js
@@ -16,15 +16,50 @@ export default function Home() {
             className={styles.profileImage}
           />
         </div>
-        <h1 className={styles.name}>John Doe</h1>
+        <h1 className={styles.name}>Jessica Randall</h1>
         <h2 className={styles.address}>London, United Kingdom</h2>
         <p className={styles.bio}>"Front-end developer and avid reader."</p>
         <div className={styles.buttonList}>
-          <button className={styles.button}>GitHub</button>
-          <button className={styles.button}>Frontend Mentor</button>
-          <button className={styles.button}>LinkdIn</button>
-          <button className={styles.button}>Twitter</button>
-          <button className={styles.button}>Instagram</button>
+          <a
+            className={styles.button}
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className={styles.button}
+            href="https://www.frontendmentor.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          <a
+            className={styles.button}
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className={styles.button}
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            className={styles.button}
+            href="https://instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
         </div>
       </div>
     </main>
